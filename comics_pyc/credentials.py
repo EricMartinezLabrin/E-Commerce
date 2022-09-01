f14387bdb9217328aed0ceb8645e4069bbbe0780
EricMartinezLabrin <98716898+EricMartinezLabrin@users.noreply.github.com>
2022-08-31 20:30:24 -0500

    fix function mercadopago

--- comics_pyc/credentials.py
+++ comics_pyc/credentials.py
@@ -1,5 +1,5 @@
 class Credentials():
-    def mercadopago(self):
+    def mercadopago():
         production = ''
         sand = 'TEST-1816279427628496-082518-84255c0be73596985adaf2dccacaeee1-113262566'
 
